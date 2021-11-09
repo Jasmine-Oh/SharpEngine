e8c5ab026fac0738902e06cbcd2418e4405558d0
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-11-09 15:56:05 +0100

    Moving triangle to the right

--- Program.cs
+++ Program.cs
@@ -7,61 +7,36 @@ namespace SharpEngine
 {
     class Program
     {
+        static float[] vertices = new float[] {
+            // vertex 1 x, y, z
+            -.5f, -.5f, 0f,
+            // vertex 2 x, y, z
+            .5f, -.5f, 0f,
+            // vertex 3 x, y, z
+            0f, .5f, 0f
+        };
+        
         static void Main(string[] args) {
             Window window = CreateWindow();
-            LoadTringleIntoBuffer();
+            LoadTriangleIntoBuffer();
             CreateShaderProgram();
 
             //Engine rendering loop
             while (!Glfw.WindowShouldClose(window)) {
                 Glfw.PollEvents(); // react to window changes (position etc.)
+                glClearColor(.1f, .1f, .1f, 1);
+                glClear(GL_COLOR_BUFFER_BIT);
                 glDrawArrays(GL_TRIANGLES, 0, 3);
                 glFlush();
+                //vertices[4] += 0.001f;
+                vertices[0] += 0.001f;
+                vertices[3] += 0.001f;
+                vertices[6] += 0.001f;
+                UpdateTriangleBuffer();
             }
         }
 
-        private static unsafe void LoadTringleIntoBuffer() {
-            float[] vertices = new float[] {
-                -.5f, -.5f, 0f,
-                .5f, -.5f, 0f,
-                0f, .5f, 0f
-            };
-
-            //Load the vertices into a buffer
-            var vertexArray = glGenVertexArray();
-            var vertexBuffer = glGenBuffer();
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            unsafe {
-                fixed (float* vertex = &vertices[0]) {
-                    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
-                }
-
-                glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
-            }
-
-            glEnableVertexAttribArray(0);
-        }
-
-        private static Window CreateWindow() {
-            //Initilize and configure
-            Glfw.Init();
-            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
-            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
-            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
-            Glfw.WindowHint(Hint.Decorated, true);
-            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
-            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
-            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
-
-            //Create and launch window
-            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
-            Glfw.MakeContextCurrent(window);
-            Import(Glfw.GetProcAddress);
-            return window;
-        }
-
-        private static void CreateShaderProgram() {
+        static void CreateShaderProgram() {
             //Create vertex shader
             var vertexShader = glCreateShader(GL_VERTEX_SHADER);
             glShaderSource(vertexShader, File.ReadAllText("shaders/red-triangle.vert"));
@@ -78,6 +53,41 @@ namespace SharpEngine
             glAttachShader(program, fragmentShader);
             glLinkProgram(program);
             glUseProgram(program);
+        }
+
+        static unsafe void LoadTriangleIntoBuffer() {
+            //Load the vertices into a buffer
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            UpdateTriangleBuffer();
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
+            glEnableVertexAttribArray(0);
+        }
+        
+        static unsafe void UpdateTriangleBuffer() {
+            fixed (float* vertex = &vertices[0]) {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
+            }
+        }
+
+        static Window CreateWindow() {
+            //Initialize and configure
+            Glfw.Init();
+            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+            Glfw.WindowHint(Hint.Decorated, true);
+            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
+
+            //Create and launch a window
+            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
+            Glfw.MakeContextCurrent(window);
+            Import(Glfw.GetProcAddress);
+            return window;
         }
     }
 }
